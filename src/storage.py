4c5935c3d546a8c58b515aece37741f76deb3ca6
Harsh Rana <hr2322808@gmail.com>
2026-01-15 21:37:47 +0530

    testing for the action

--- src/storage.py
+++ src/storage.py
@@ -27,7 +27,7 @@ load_dotenv()
 def get_engine():
     return create_engine(
         f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', 5432)}/{os.getenv('DB_NAME')}",
         echo=False,
     )
 
